b5d69ba612931c096e38398fc308534449b4322b
Service Web <sw@gssmail.com>
2024-03-14 16:50:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5826\GCG_5826_Error_Log.sql

---
+++ GCG_5826_Error_Log.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_5826_Error_Log"(
+ "Er_Type" CHAR(1),
+ "Er_Key" CHAR(50),
+ "Er_Title" CHAR(50),
+ "Er_Details" VARCHAR(500),
+ "Er_Date" DATETIME,
+ "Er_User" CHAR(50),
+ "Er_Terminal" CHAR(3) );
